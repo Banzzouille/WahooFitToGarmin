a378c0f1438b16fd762f0140893a57d86ab3b18c
Florian BARLETTA <florian.barletta@gmail.com>
2021-03-08 18:59:15 +0100

    Initial Commit

---
+++ WahooFitToGarmin/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace WahooFitToGarmin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+
+            return "Hello world !";
+        }
+
+    }
+}

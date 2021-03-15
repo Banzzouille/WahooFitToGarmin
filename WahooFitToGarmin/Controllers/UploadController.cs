69176e2f65292f17f180f387c1ed1f3fc9878de4
BanZZouille <djflolecrapo@hotmail.com>
2021-03-15 18:15:10 +0100

    Try another solution

---
+++ WahooFitToGarmin/Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace WahooFitToGarmin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UploadController : Controller
+    {
+        private readonly ILogger<UploadController> _logger;
+
+        public UploadController(ILogger<UploadController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var rng = new Random();
+            return $"Coucou {rng.Next(int.MinValue, int.MaxValue)}";
+        }
+    }
+}
--- WahooFitToGarmin/Startup.cs
+++ WahooFitToGarmin/Startup.cs
@@ -1,16 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WahooFitToGarmin
 {
@@ -28,10 +26,6 @@ namespace WahooFitToGarmin
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WahooFitToGarmin", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +34,7 @@ namespace WahooFitToGarmin
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WahooFitToGarmin v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 

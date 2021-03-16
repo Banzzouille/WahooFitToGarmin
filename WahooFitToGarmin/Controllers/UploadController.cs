e8b4839f739f5d9d83a7006762d5d7342f0a143b
BanZZouille <djflolecrapo@hotmail.com>
2021-03-16 09:37:44 +0100

    Add some logs on call

--- WahooFitToGarmin/Controllers/UploadController.cs
+++ WahooFitToGarmin/Controllers/UploadController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 using WahooFitToGarmin.Services;
 
 namespace WahooFitToGarmin.Controllers
@@ -19,17 +21,32 @@ namespace WahooFitToGarmin.Controllers
         }
 
         [HttpGet]
-        public ActionResult<string> Get(string challenge)
+        public ActionResult<string> Verify(string challenge)
         {
+            _logger.Log(LogLevel.Information, $"Enter Verify GET method");
             _logger.Log(LogLevel.Information,$"challenge received : {challenge}");
+            _logger.Log(LogLevel.Information, $"========================================================");
             return challenge;
         }
 
         [HttpPost]
-        public ActionResult GetNotification([FromBody] string dataReceive)
+        public ActionResult<string> GetNotification([FromBody] string dataReceived)
         {
-            _logger.Log(LogLevel.Information, $"data received : {dataReceive}");
-            return Ok();
+            _logger.Log(LogLevel.Information, $"Enter GetNotification POST method");
+            // Receive a list of changed user IDs from Dropbox and process each.
+
+
+            // Make sure this is a valid request from Dropbox
+            StringValues headerValue;
+            Request.Headers.TryGetValue("X-Dropbox-Signature", out headerValue);
+            var headerValueResult = headerValue.FirstOrDefault();
+            _logger.Log(LogLevel.Information, $"headerValueResult: {headerValueResult}");
+
+
+
+            _logger.Log(LogLevel.Information, $"dataReceived: {dataReceived}");
+            _logger.Log(LogLevel.Information, $"========================================================");
+            return "";
         }
 
 

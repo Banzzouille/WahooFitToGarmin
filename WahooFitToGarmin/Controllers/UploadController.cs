a0942beb938b3dca72970d59df59ccf74dd672de
BanZZouille <djflolecrapo@hotmail.com>
2021-03-17 18:52:14 +0100

    Now we can download new fit files

--- WahooFitToGarmin/Controllers/UploadController.cs
+++ WahooFitToGarmin/Controllers/UploadController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Dropbox.Api;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
@@ -70,12 +71,52 @@ namespace WahooFitToGarmin.Controllers
                     return Forbid();
             }
 
-            // Do your thing here... e.g. store it in a queue to process later
-            // ...
+            await ListRootFolder(GetClient());
 
-            // Return A-OK :)
             return Ok();
 
+        }
+
+        async Task ListRootFolder(DropboxClient dbx)
+        {
+            var list = await dbx.Files.ListFolderAsync(string.Empty, true, true);
+
+            // show folders then files
+            foreach (var item in list.Entries.Where(i => i.IsFolder))
+            {
+                if(!item.Name.Equals("WahooFitness")) continue;
+                Console.WriteLine("D  {0}/", item.Name);
+
+                var files = await dbx.Files.ListFolderAsync(item.PathLower);
+                foreach (var file in files.Entries.Where(i => i.IsFile))
+                {
+                    if(!file.Name.EndsWith(".fit")) continue;
+                    Console.WriteLine("F{0,8} {1}", file.AsFile.Size, file.Name);
+                    using (var response = await dbx.Files.DownloadAsync(file.PathLower))
+                    {
+                        if (!System.IO.Directory.Exists("Activities"))
+                            System.IO.Directory.CreateDirectory("Activities");
+
+                        if (!System.IO.File.Exists(Path.Combine("Activities", file.Name)))
+                            System.IO.File.Delete(Path.Combine("Activities", file.Name));
+
+                        _logger.LogInformation($"{DateTime.Now} ==> downloading file: {file.PathLower}");
+                        var fileContent = await response.GetContentAsByteArrayAsync();
+                        await System.IO.File.WriteAllBytesAsync(Path.Combine("Activities", file.Name), fileContent);
+
+                        //_logger.LogInformation($"{DateTime.Now} ==> deleting file: {file.PathLower}");
+                        //await dbx.Files.DeleteV2Async(file.PathLower);
+                    }
+                }
+            }
+        }
+
+        private DropboxClient GetClient()
+        {
+            var currentClient = new DropboxClient(
+                _dropboxSettingsService.GetDropboxAppToken(), new DropboxClientConfig(_dropboxSettingsService.GetDropboxAppName()));
+
+            return currentClient;
         }
         private string GetSha256Hash(HMACSHA256 sha256Hash, string input)
         {

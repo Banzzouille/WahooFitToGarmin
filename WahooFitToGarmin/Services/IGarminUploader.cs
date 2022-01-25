7b487a0bd84f94991d413f0478dcffdf60d395ea
BanZZouille <djflolecrapo@hotmail.com>
2022-01-25 10:29:21 +0100

    use dotnet code instead of python to upload package

---
+++ WahooFitToGarmin/Services/IGarminUploader.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace WahooFitToGarmin.Services
+{
+    public interface IGarminUploader
+    {
+        Task UploadAsync(string garminLogin, string garminPwd, string filePath, ILogger logger);
+    }
+}
